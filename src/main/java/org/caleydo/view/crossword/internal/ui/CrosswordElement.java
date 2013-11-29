714c6ad1b63c34e454fa519c8892a3edafcf1e8b
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-11-29 16:06:40 +0100

    use drag detected event

--- src/main/java/org/caleydo/view/crossword/internal/ui/CrosswordElement.java
+++ src/main/java/org/caleydo/view/crossword/internal/ui/CrosswordElement.java
@@ -262,6 +262,11 @@ public class CrosswordElement extends AnimatedGLElementContainer implements
 				info.setHovered(info.isSelected());
 			}
 			break;
+		case DRAG_DETECTED:
+			if (((isToolBar && !event.isCtrlDown()) || (!isToolBar && event.isCtrlDown())) && !pick.isAnyDragging()) {
+				pick.setDoDragging(true);
+			}
+			break;
 		case CLICKED:
 			if (((isToolBar && !event.isCtrlDown()) || (!isToolBar && event.isCtrlDown())) && !pick.isAnyDragging()) {
 				pick.setDoDragging(true);
